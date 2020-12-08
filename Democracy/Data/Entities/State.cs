25d459ed8b32ec3c9c91e625e9dca961a07450b1
gerardo lanza <glanza007@gmail.com>
2020-12-08 11:58:29 -0600

    States Controller and automatic migration enabled

---
+++ Democracy/Data/Entities/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Democracy.Data.Entities
+{
+    public class State
+    {
+        [Key]
+        public int StateId { get; set; }
+
+        [Required(ErrorMessage = "Debes ingresar una {0}")]
+        [StringLength(30, ErrorMessage =
+            "El campo {0} puede contener máximo {1} y mínimo {2} caráteres",
+            MinimumLength = 3)]
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+
+    }
+}
